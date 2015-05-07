93fe9a2a0abad91d55367e3370fc82f7d5eb4b0f
ciregbulem <chudi.iregbulem@trincoll.edu>
2015-05-07 14:44:34 -0400

    Increased character limit for Notifications and added Admin login button

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification < ActiveRecord::Base
 	validates_presence_of :headline
 	validates_presence_of :content
-	validates_length_of :headline, maximum: 30
-	validates_length_of :content, maximum: 75
+	validates_length_of :headline, maximum: 50
+	validates_length_of :content, maximum: 140
 	belongs_to :admin
 end
